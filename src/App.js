43119b9132c8cffa7408b0ce8db46222446b66ea
Minh <nguyentrongminh1412@gmail.com>
2022-04-19 23:59:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <>
       <CssBaseline>
+        <Header />
         <Grid container spacing={3} style={{ width: '100%' }}>
           <Grid item xs={12} md={4}>
             <List />
