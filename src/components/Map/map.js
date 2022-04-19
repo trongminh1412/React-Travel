1635b74f10c649ba64865e3dd939be3e9cdc4bcf
Minh <nguyentrongminh1412@gmail.com>
2022-04-20 00:00:40 +0700

    update component Map

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const map = () => {
-  return <div>map</div>;
+import useStyles from './styles';
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={coordinates}
+      ></GoogleMapReact>
+    </div>
+  );
 };
-export default map;
+export default Map;
