0ceae00dbb144d7c9aae1c70405bf70a732ea1e4
Minh <nguyentrongminh1412@gmail.com>
2022-04-21 17:55:59 +0700

    component list

--- src/components/List/list.js
+++ src/components/List/list.js
@@ -1,6 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const list = () => {
-  return <div>list</div>;
+import PlaceDetails from '../PlaceDetails/placeDetails';
+
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+
+import useStyles from './styles';
+
+const List = () => {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState(0);
+
+  const places = [
+    { name: 'Cool Place' },
+    { name: 'Best Beer' },
+    { name: 'Best Steak' },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attraction around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attraction">Attraction</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={5}>Above 5.0</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
-export default list;
+export default List;
