995c6d5d40783bd9fb1d6b572229966810b98a24
Minh <nguyentrongminh1412@gmail.com>
2022-04-21 17:56:44 +0700

    update component Map with google map api

--- src/components/Map/map.js
+++ src/components/Map/map.js
@@ -6,17 +6,24 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
-
-  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
+        bootstrapURLKeys={{ key: 'AIzaSyDtzESRllIzvJV_aXa3cD1jp3TDBlWlGjE' }}
         defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+        }}
+        onChildClick={''}
       ></GoogleMapReact>
     </div>
   );
