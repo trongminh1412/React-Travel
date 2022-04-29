9bad166c13b01326768079d1cece7114a13bf907
Minh <nguyentrongminh1412@gmail.com>
2022-04-29 21:04:21 +0700

    fix bug Cannot read properties of null (reading 'sw') and ne

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [places, setPlaces] = useState([]);
 
   const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
-  const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
