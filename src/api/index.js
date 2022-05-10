9f25d62630cd8449fda7164130a0a39b6339e8ff
Minh <nguyentrongminh1412@gmail.com>
2022-05-10 23:56:32 +0700

    update type in api

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
+import { type } from '@testing-library/user-event/dist/type';
 import axios from 'axios';
 
-const URL =
-  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        tr_latitude: ne.lat,
-      },
-      headers: {
-        'x-rapidapi-key': 'f72fd6f314msh2d07be597d7cf45p1d62f2jsn29dcdcb894b4',
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          'x-rapidapi-key':
+            'f72fd6f314msh2d07be597d7cf45p1d62f2jsn29dcdcb894b4',
+          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        },
+      }
+    );
 
     return data;
   } catch (error) {
