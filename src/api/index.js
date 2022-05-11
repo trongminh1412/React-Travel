09556544618a2fe2a64e491ff10bec08ba322a47
Minh <nguyentrongminh1412@gmail.com>
2022-05-11 16:42:31 +0700

    update api weather

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,3 @@
-import { type } from '@testing-library/user-event/dist/type';
 import axios from 'axios';
 
 export const getPlacesData = async (type, sw, ne) => {
@@ -15,13 +14,38 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_latitude: ne.lat,
         },
         headers: {
-          'x-rapidapi-key':
-            'f72fd6f314msh2d07be597d7cf45p1d62f2jsn29dcdcb894b4',
+          // 'x-rapidapi-key':
+          //   'f72fd6f314msh2d07be597d7cf45p1d62f2jsn29dcdcb894b4',
+          
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
           'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
         },
       }
     );
 
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      'https://community-open-weather-map.p.rapidapi.com/find',
+      {
+        params: {
+          lat,
+          lon: lng,
+        },
+        headers: {
+          // 'x-rapidapi-key':
+          //   'f72fd6f314msh2d07be597d7cf45p1d62f2jsn29dcdcb894b4',           
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host':'community-open-weather-map.p.rapidapi.com',
+        },
+      }
+    );
     return data;
   } catch (error) {
     console.log(error);
